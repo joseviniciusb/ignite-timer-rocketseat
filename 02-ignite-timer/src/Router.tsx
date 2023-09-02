82e10e042b7518a4f0e7ca9da423cc0790f4a0ea
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-09-02 18:39:58 +0000

    feat: abraçar as rotas com um layout padrão

--- 02-ignite-timer/src/Router.tsx
+++ 02-ignite-timer/src/Router.tsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import History from "./pages/History";
+import { DefautLayout } from "./layouts/DefaultLayout";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/history" element={<History />}/>
+      <Route path="/" element={<DefautLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   );
 };
