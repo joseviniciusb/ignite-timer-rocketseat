c388f017a274a662f4753b07a079fc47fc8b4e76
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-09-02 18:40:20 +0000

    ft: excluir componentes usados como exemplo

--- 02-ignite-timer/src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, buttonVariant } from "./Button.styles";
-
-interface ButtonProps {
-  variant?: buttonVariant;
-}
-
-export const Button = ({ variant = "primary" }: ButtonProps) => {
-  return <ButtonContainer variant={variant} />;
-};
