ac01d71cb8544c08365bbec96298fad50ae96b1e
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-09-02 16:43:25 +0000

    feat: configurar eslint json

--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -3,12 +3,11 @@ import styled from "styled-components";
 export type buttonVariant = "primary" | "secondary" | "danger" | "success";
 
 interface ButtonContainerProps {
-    variant: buttonVariant ;
+  variant: buttonVariant;
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-width: 100px;
-height: 100px;
-background-color: ${props=> props.theme['green-500']}
-
-`
+  width: 100px;
+  height: 100px;
+  background-color: ${(props) => props.theme["green-500"]};
+`;
