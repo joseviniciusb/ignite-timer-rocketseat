287b21d0d6c160359afc6fb7a9e501fd4df148e8
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-09-06 22:40:08 +0000

    feat: salvar o estado no localstorage

--- 02-ignite-timer/src/contexts/CyclesContext.tsx
+++ 02-ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useReducer, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 
 import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 import {
@@ -6,6 +12,7 @@ import {
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
 } from "../reducers/actions";
+import { differenceInSeconds } from "date-fns";
 
 interface CreateCycleData {
   task: string;
@@ -32,17 +39,42 @@ interface CyclesContextProviderProps {
 export const CyclesContextProvider = ({
   children,
 }: CyclesContextProviderProps) => {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storageStateJSON = localStorage.getItem(
+        "@igniteTimer:cycles-state-1.0.0"
+      );
+      if (storageStateJSON) {
+        return JSON.parse(storageStateJSON);
+      }
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      return initialState;
+    }
+  );
+
   const { cycles, activeCycleId } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => {
     return cycle.id === activeCycleId;
   });
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+    localStorage.setItem("@igniteTimer:cycles-state-1.0.0", stateJSON);
+  }, [cyclesState]);
 
   const setSecondsPast = (seconds: number) => {
     setAmountSecondsPassed(seconds);
